c39f55487688aa262b154b48df5e7f046ab51eea
ArtyomKopan <artem.kopan.2003@mail.ru>
2022-03-30 15:42:36 +0300

    I added detekt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ val jvmTargetVersion: String by project
 
 plugins {
     kotlin("jvm")
+    id("io.gitlab.arturbosch.detekt") version "1.19.0"
     java
 }
 
@@ -13,6 +14,15 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.19.0")
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(kotlin("test"))
+}
+
 
 allprojects {
     apply {
